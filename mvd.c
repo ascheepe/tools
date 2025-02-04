74d4f82a8f4f991c14855aab6e1cb44784156369
Axel Scheepers <no@spam.please>
2025-02-04 15:40:35 +0100

    make reading the current directory part of the program so that big directories don't cause shell argument list overflow

--- mvd.c
+++ mvd.c
@@ -4,11 +4,13 @@
 #include <string.h>
 #include <time.h>
 
+#include <dirent.h>
 #include <err.h>
 #include <errno.h>
 #include <libgen.h>
 #include <limits.h>
 #include <sys/stat.h>
+#include <sys/types.h>
 #include <unistd.h>
 
 static char errstr[1024];
@@ -59,33 +61,50 @@ mvd(char *file, char *fmt)
 void
 usage(void)
 {
-	fputs("usage: mvd [-f fmt] file [file ...]\n", stderr);
+	fputs("usage: mvd [-f fmt]\n", stderr);
 	exit(1);
+}
+
+int
+isdir(char *name)
+{
+	struct stat sb;
+
+	if (stat(name, &sb) == -1)
+		errx(1, "%s: %s.", name, strerror(errno));
+
+	return S_ISDIR(sb.st_mode);
 }
 
 int
 main(int argc, char **argv)
 {
 	char *fmt = "%Y%m";
-	int i, opt;
+	struct dirent *de;
+	DIR *dirp;
+	int opt;
 
 	while ((opt = getopt(argc, argv, "f:")) != -1) {
 		switch (opt) {
 		case 'f':
 			fmt = optarg;
 			break;
+		case 'h':
 		default:
 			usage();
 		}
 	}
 
-	if (optind >= argc)
-		usage();
+	if ((dirp = opendir(".")) == NULL)
+		errx(1, NULL);
 
-	for (i = optind; i < argc; ++i) {
-		if (mvd(argv[i], fmt) == -1)
+	while ((de = readdir(dirp)) != NULL) {
+		if (isdir(de->d_name))
+			continue;
+		if (mvd(de->d_name, fmt) == -1)
 			errx(1, "%s", errstr);
 	}
 
+	closedir(dirp);
 	return 0;
 }
