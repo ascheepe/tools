d94a40196fb23a700bbd474de506aa0edc51a23c
Axel Scheepers <no@spam.please>
2026-09-07 14:34:40 +0200

    add python version of shuffle but without libmagic

---
+++ shuffle.py
@@ -0,0 +1,66 @@
+#! /usr/bin/env python3
+
+import argparse
+import os
+import random
+import subprocess
+import sys
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        description="Run a command for each file in random order."
+    )
+
+    parser.add_argument(
+        "-e", "--extension", type=str, help="Find files with this extension."
+    )
+
+    parser.add_argument(
+        "-p", "--path", default=".", help="Find files in this path."
+    )
+
+    parser.add_argument(
+        "command", nargs="+", help="Run this command for each file."
+    )
+
+    args = parser.parse_args()
+
+    if args.extension:
+        if not args.extension.startswith("."):
+            args.extension = "." + args.extension
+        args.extension = args.extension.lower()
+
+    file_list = []
+    for root, dirs, files in os.walk(args.path):
+        for file in files:
+            if args.extension:
+                _, extension = os.path.splitext(file)
+                if args.extension != extension.lower():
+                    continue
+            fullname = os.path.join(root, file)
+            file_list.append(fullname)
+
+    random.shuffle(file_list)
+
+    for file in file_list:
+        append_file = True
+        has_placeholder = any("{}" in arg for arg in args.command)
+        command = [arg.replace("{}", file) for arg in args.command]
+        if not has_placeholder:
+            command.append(file)
+
+        print(f"==> {" ".join(command)}")
+        try:
+            subprocess.run(command, capture_output=True, check=True)
+        except Exception as e:
+            print(e, file=sys.stderr)
+            return 1
+        except:
+            return 1
+
+    return 0
+
+
+if __name__ == "__main__":
+    sys.exit(main())
