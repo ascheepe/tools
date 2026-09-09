dae4138ce64f054da1d0a1c8fafc61d1e6b27648
Axel Scheepers <no@spam.please>
2026-09-09 15:30:46 +0200

    add perl version of shuffle

---
+++ shuffle.pl
@@ -0,0 +1,52 @@
+use warnings;
+use strict;
+use autodie qw(:all);
+
+use File::Find;
+use Getopt::Long;
+use List::Util qw(shuffle);
+
+sub main {
+    my $path = ".";
+    my $extension;
+    my @command;
+    my @files;
+
+    GetOptions(
+        "extension=s" => \$extension,
+        "path=s"      => \$path,
+    ) or die("Error in command line arguments\n");
+
+    if ( defined($extension) && $extension !~ /^\./ ) {
+        $extension = lc(".$extension");
+    }
+
+    File::Find::find(
+        sub {
+            return if !-f;
+            if ( defined($extension) ) {
+                return if ( lc($_) !~ /$extension$/ );
+            }
+            push( @files, $File::Find::name );
+        },
+        $path
+    );
+
+    @files = shuffle @files;
+
+    foreach my $file (@files) {
+        @command = @ARGV;
+        my $has_placeholder = grep { /\{\}/ } @command;
+        if ($has_placeholder) {
+            map { s/\{\}/$file/ } @command;
+        }
+        else {
+            push( @command, $file );
+        }
+
+        print("==> @command\n");
+        system(@command);
+    }
+}
+
+main();
